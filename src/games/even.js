d77e44f46c19fa853b0975312fb9c3f318d0dd2c
sergeiwerty <dshl.srp@gmail.com>
2019-12-27 08:06:18 +0300

    step5_edits

--- src/games/even.js
+++ src/games/even.js
@@ -1,5 +1,8 @@
 import readlineSync from 'readline-sync';
 import { nameRequest } from '../index';
+import greeting from '../index';
+
+const greetingTheUser = greeting();
 
 const evenGame = () => {
   const generateRandomNum = () => {
@@ -18,17 +21,20 @@ const evenGame = () => {
   };
 
   const evenGameResult = () => {
-    for (let i = 0; i <= 3; i += 1) {
+    for (let i = 1; i < 4; i += 1) {
       const randomizedNum = generateRandomNum();
       const rightAnswer = isOddNumber(randomizedNum);
       const answerRequest = readlineSync.question(`Question: ${randomizedNum}`);
       if (answerRequest !== rightAnswer) {
         console.log(`${answerRequest} is wrong answer ;(. Correct answer was ${rightAnswer}.\nLet's try again, ${nameRequest}!`);
+        break;
       } else {
         console.log('Correct!');
+        if (i === 3) {
+          console.log(`Congratulations, ${nameRequest}`);
+        }
       }
     }
-    return (`Congratulations, ${nameRequest}`);
   };
   return evenGameResult();
 };
--- src/index.js
+++ src/index.js
@@ -5,7 +5,6 @@ export const nameRequest = readlineSync.question('May I have your name? ');
 const greeting = () => {
   console.log('Welcome to the Brain Games!');
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
-  // nameRequest = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${nameRequest}!`);
 };
 export default greeting;
